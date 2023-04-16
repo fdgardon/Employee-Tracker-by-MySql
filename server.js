35ced63a38f95ba0a70d649efc528a0fc4f91f96
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-15 22:00:04 -0700

    department codes are added

--- server.js
+++ server.js
@@ -3,45 +3,66 @@ const connect = require('./connection');
 
 AskQuestion();
 
-function AskQuestion(){
+function AskQuestion() {
   inquirer.prompt([
     {
-        name: "prompt",
-        type: "list",
-        message: "EMPLOYEE MANAGER\n What would you like to do?",
-        choices: [
-          "Show all departments",
-          "Add a department",
-          "Show all roles",
-          "Add a role",
-          "Show all employees",
-          "Add an employee",
-          "Update an employee role",
-          "I am done",
-        ],
+      name: "prompt",
+      type: "list",
+      message: "EMPLOYEE MANAGER\n What would you like to do?",
+      choices: [
+        "Show all departments",
+        "Add a department",
+        "Show all roles",
+        "Add a role",
+        "Show all employees",
+        "Add an employee",
+        "Update an employee role",
+        "I am done",
+      ],
     },
   ])
 
-  .then(function(answer){
-    switch (answer.prompt){
+    .then(function (answer) {
+      switch (answer.prompt) {
         case 'Show all departments':
-            viewAllDep();
-            break;
-    }
-  })
+          viewAllDep();
+          break;
+        case 'Add a department':
+          addDepartment()
+          break;
 
-      
+      }
+
+    })
 };
 
 const viewAllDep = () => {
-    db.query("SELECT * FROM department", (err, res) => {
-      if (err) throw err;
-      console.table(res);
-      Question();
-    });
-  };
-  
+  connect.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    AskQuestion();
+  });
+};
+const addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: 'What is the name of Department?',
+      name: 'name'
 
-    
+    }
+  ])
+    .then((response) => {
+      let sqlQuery = 'INSERT INTO department SET ?';
+      connect.query(sqlQuery, response, function (error, results) {
+        if (error) throw error;
+        console.log('New department has been successfully created!');
 
-    
+        AskQuestion();
+      });
+    })
+};
+
+
+
+
